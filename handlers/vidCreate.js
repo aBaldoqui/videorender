2910f8e165bfce4e8acfdee37bb9a5bd025ce4c2
Arthur B Bergamo <arthur.b.bergamo@gmail.com>
2022-10-09 09:51:19 -0300

    import audio file

--- handlers/vidCreate.js
+++ handlers/vidCreate.js
@@ -1,25 +1,42 @@
 const ae = require('after-effects')
-
+const path = require('path')
 //https://ae-scripting.docsforadobe.dev/other/importoptions.html#importoptions
 //err
-const importOptions = new ImportOptions();
-importOptions.file = new File('C:\Users\arthu\Documents\dev\videorender\tmp\neurocientista_sobre_o_vicio_em_p0rn0gr4f1a__eslen_delanogare____cortes_podcast.mp3');
+
 
 async function index() {
-
     const create_composition = new ae.Command(name => {
         name = typeof name === "string" ? name : "New Comp";
         app.project.items.addComp(name, 1920, 1080, 1, 24, 10);
     });
 
-    const importAudiofile = ae.Command(importOptions => {
-        app.project.importFile(importOptions)
-    })
 
-    ae(create_composition, "First Comp");
-    ae(importAudiofile, importOptions)
-    
-    
+    let audioFile = path.resolve('..', 'tmp', 'neurocientista_sobre_o_vicio_em_p0rn0gr4f1a__eslen_delanogare____cortes_podcast.mp3').replace('C:','')
+
+    //audioFile = `\\${audioFile}`     
+
+    try {
+        ae.execute((audioFile_fromnode) => {
+            const importOptions = new ImportOptions()
+            importOptions.file = new File('~/Documents/dev/videorender/tmp/neurocientista_sobre_o_vicio_em_p0rn0gr4f1a__eslen_delanogare____cortes_podcast.mp3');
+            app.project.importFile(importOptions)
+        }, audioFile);
+    } catch (err) {
+        console.log(err)
+    }
+
+
+
+    // const importAudiofile = new ae.Command(audioFile => {
+    //     const importOptions = new ImportOptions()
+    //     importOptions.file = new File(audioFile);
+    //     app.project.importFile(importOptions)
+    // })
+
+    // //ae(create_composition, "First Comp");
+    // ae(importAudiofile, audioFile)
+
+
 }
 
 
